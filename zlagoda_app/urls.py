4a3a4462d055d49ccdd5ec2e33054350dce32c3c
Vladyslava Tytarenko <vladislavaaa.022@gmail.com>
2025-05-05 19:38:53 +0300

    New dashboard updates

--- zlagoda_app/urls.py
+++ zlagoda_app/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.contrib.auth.views import LogoutView
 from . import views
 
 urlpatterns = [
     path('', views.check_login, name='login'),
     path('manager/', views.manager_dashboard, name='manager_dashboard'),
     path('cashier/', views.cashier_dashboard, name='cashier_dashboard'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
 ]
